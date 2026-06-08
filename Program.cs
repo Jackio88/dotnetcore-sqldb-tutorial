0445b403dd533d6aefc05453f7ecb79dfb19770f
Jackio88 <jlu4987@gmail.com>
2026-06-08 16:13:39 +1200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,32 +2,28 @@ using DotNetCoreSqlDb.Models;
 using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 builder.Logging.AddAzureWebAppDiagnostics();
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<MyDatabaseContext>(options =>
-                    options.UseSqlServer(builder.Configuration.GetConnectionString("MyDbConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MyDbConnection")));
 var app = builder.Build();
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
 }
-else {
+else
+{
     app.UseExceptionHandler("/Error");
     app.UseHsts();
 }
-// app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Todos}/{action=Index}/{id?}");
-
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<MyDatabaseContext>();
     db.Database.Migrate();
 }
-
 app.Run();
